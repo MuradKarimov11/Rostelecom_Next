53c72962d8c00037ba1dbd26ee80fcf0f9576fb6
Murad Karimov <muradkarimov07@gmail.com>
2025-01-15 04:38:24 +0300

    created project Rostelecom-shop. Add Modules Header.tsx

---
+++ components/elements/Logo/Logo.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
+
+import Link from "next/link";
+
+const Logo = () => (
+  <Link className="logo" href="/">
+    <img className="logo__img" src="/img/logo.svg" alt="Rostelecom Logo" />
+  </Link>
+);
+
+export default Logo;
